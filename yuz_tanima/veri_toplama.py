7d868e77670f9ac6909cb7758f9842da46b3fc51
Rabia DAGDEMİR <48928879+rabiadagdemir@users.noreply.github.com>
2020-02-05 00:33:24 +0300

    Update veri_toplama.py

--- yuz_tanima/veri_toplama.py
+++ yuz_tanima/veri_toplama.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 #cam.set(3, 640)
 #cam.set(4, 480)
 face_detector = cv2.CascadeClassifier("Cascade/haarcascade_frontalface_default.xml")
